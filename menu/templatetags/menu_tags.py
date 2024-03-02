e56d277d9df3ea4103f8b2cf050c03bdaa71325a
Artem Sitd <tym3@yandex.ru>
2024-03-02 12:20:05 +0500

    readme

--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -6,18 +6,19 @@ register = template.Library()
 
 
 @register.inclusion_tag("menu.html")
-def draw_menu():
-    categories = Category.objects.filter(parent__isnull=True).prefetch_related(
-        "subcategory"
-    )
+def draw_menu(menu_name):
+    if menu_name == 'main_menu':
+        categories = Category.objects.filter(parent__isnull=True).prefetch_related(
+            "subcategory"
+        )
 
-    def add_subcategories(category, level=0):
-        category.has_children = bool(category.subcategory.all())
-        category.level = level
-        if category.has_children:
-            for subcategory in category.subcategory.all():
-                add_subcategories(subcategory, level + 1)
+        def add_subcategories(category, level=0):
+            category.has_children = bool(category.subcategory.all())
+            category.level = level
+            if category.has_children:
+                for subcategory in category.subcategory.all():
+                    add_subcategories(subcategory, level + 1)
 
-    for category in categories:
-        add_subcategories(category)
-    return {"categories": categories}
+        for category in categories:
+            add_subcategories(category)
+        return {"categories": categories}
