2c3ba6331206f692c6dcec399f3a7390ccaaebec
Artem Sitd <tym3@yandex.ru>
2024-03-02 11:47:11 +0500

    настройка всех url

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-# from .views import get_main, get_about, get_contacts, get_history, get_portfolio, get_team, category_detail
 from .views import CategoryDetail
 
 app_name = "menu"
@@ -8,4 +7,3 @@ app_name = "menu"
 urlpatterns = [
     path("<slug:slug>/", CategoryDetail.as_view(), name="category_detail"),
 ]
-
